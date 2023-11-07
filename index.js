a121b2b050dec4e70504429aa991490cf5843162
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-07 17:43:09 -0500

    connect index to product

--- index.js
+++ index.js
@@ -4,8 +4,10 @@ const path = require("path");
 
 const mongoose = require("mongoose");
 
+const Product = require("./models/product");
+
 mongoose
-  .connect("mongodb://127.0.0.1:27017/movieApp")
+  .connect("mongodb://127.0.0.1:27017/farmStand")
   .then(() => {
     console.log("mongo connected!!");
   })
