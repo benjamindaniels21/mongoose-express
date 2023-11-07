d85e0d732b0303e1ba1eafe2b1821ed77de9aae5
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-07 17:36:42 -0500

    setting up mongoose

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/movieApp")
+  .then(() => {
+    console.log("mongo connected!!");
+  })
+  .catch((err) => {
+    console.log("mongo error!!");
+    console.log(err);
+  });
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
@@ -9,6 +21,6 @@ app.get("/dog", (req, res) => {
   res.send("WOOF!");
 });
 
-app.listen(3000, () => {
+app.listen(5000, () => {
   console.log("app is listening on port 3000");
 });
