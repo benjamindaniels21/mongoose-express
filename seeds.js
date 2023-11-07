7ff5e12af2415ff997e051f3f9ac0d80645692a9
Ben Daniels <benjamindaniels21@gmail.com>
2023-11-07 18:00:28 -0500

    adding seed data array

--- seeds.js
+++ seeds.js
@@ -11,16 +11,50 @@ mongoose
     console.log(err);
   });
 
-const p = new Product({
-  name: "Grapefruit",
-  price: 1.99,
-  category: "fruit",
-});
+// const p = new Product({
+//   name: "Grapefruit",
+//   price: 1.99,
+//   category: "fruit",
+// });
 
-p.save()
-  .then((p) => {
-    console.log(p);
+// p.save()
+//   .then((p) => {
+//     console.log(p);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const seedProducts = [
+  {
+    name: "Fairy Eggplant",
+    price: 1.0,
+    category: "vegetable",
+  },
+  {
+    name: "Melon",
+    price: 4.99,
+    category: "fruit",
+  },
+  {
+    name: "Seedless Watermelon",
+    price: 3.99,
+    category: "fruit",
+  },
+  {
+    name: "Organic Celery",
+    price: 1.5,
+    category: "vegetable",
+  },
+  {
+    name: "Chocolate Whole Milk",
+    price: 2.69,
+    category: "dairy",
+  },
+];
+
+Product.insertMany(seedProducts)
+  .then((res) => {
+    console.log(res);
   })
-  .catch((e) => {
-    console.log(e);
-  });
+  .catch((err) => console.log(err));
